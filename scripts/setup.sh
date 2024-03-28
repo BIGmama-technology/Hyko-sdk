7a4a7b1ce4deed6103adf50e3065a53039099dec
1hachem <hb@big-mama.io>
2024-03-28 10:59:55 +0100

    ci: setup pre-push hooks in setup.py

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ pyenv install
 
 poetry install
 
-poetry run pre-commit install
+poetry run pre-commit install --hook-type pre-commit --hook-type pre-push
 poetry run gitlint install-hook
 
 poetry shell
